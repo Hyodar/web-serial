d771b65840b4990c11297fba9f9a92cb531b3f23
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2020-06-13 22:42:07 -0300

    Add send message button

--- web-serial/src/components/SerialInput.vue
+++ web-serial/src/components/SerialInput.vue
@@ -1,13 +1,20 @@
 <template>
   <v-container class="grey darken-3" style="height: 150px;">
-    <v-textarea
-      v-model="message"
-      outlined
-      hide-details
-      autocomplete="off"
-      height="130px"
-      @keyup.enter.exact="sendMessage()"
-    ></v-textarea>
+    <v-row align="center">
+      <v-textarea
+        v-model="message"
+        outlined
+        hide-details
+        autocomplete="off"
+        height="130px"
+        @keyup.enter.exact="sendMessage"
+      ></v-textarea>
+      <v-btn icon v-on:click="sendMessage" color="primary" x-large>
+        <v-icon>
+          mdi-send
+        </v-icon>
+      </v-btn>
+    </v-row>
   </v-container>
 </template>
 
@@ -21,8 +28,10 @@ export default {
 
   methods: {
     sendMessage() {
-      this.$emit("sendMessage", this.message);
-      this.message = "";
+      if (this.message) {
+        this.$emit("sendMessage", this.message);
+        this.message = "";
+      }
     }
   }
 };
