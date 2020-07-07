02503203ec7f3a3add5028e4f0d10fad651ce541
Franco Barpp Gomes <franco.b.gomes@gmail.com>
2020-07-07 12:17:53 -0300

    Fix extra space on link text

--- web-serial/src/components/navigationDrawer/BottomInfo.vue
+++ web-serial/src/components/navigationDrawer/BottomInfo.vue
@@ -1,7 +1,7 @@
 <template>
   <NavigationDrawerRow>
     <div class="text-caption text-center pa-3">
-      Made by <a href="https://github.com/Hyodar"> Hyodar </a> and powered
+      Made by <a href="https://github.com/Hyodar">Hyodar</a> and powered
       by Vue.js and Vuetify.
     </div>
   </NavigationDrawerRow>
